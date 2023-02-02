6dc0e41e773a2ad93037f72a351824fe5ddc434e
basbky <markbasbky@gmail.com>
2023-02-03 00:28:37 +0700

    django settings configured

--- src/config/settings.py
+++ src/config/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('DB_USER', default='postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='localhost'),
         'PORT': os.getenv('DB_PORT', default='5432'),
     }
 }
