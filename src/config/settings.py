035c739005d650c91ce2207bdf4df5c00dff43fb
basbky <markbasbky@gmail.com>
2023-02-03 20:08:23 +0700

    dependencies added

--- src/config/settings.py
+++ src/config/settings.py
@@ -40,12 +40,16 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    # Django default
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Django add-ons
+    'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
