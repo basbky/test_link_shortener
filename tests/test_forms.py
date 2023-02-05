3075982aa40a2ee6780dc521c5b9e1620a17fdfa
basbky <markbasbky@gmail.com>
2023-02-06 04:12:33 +0700

    tests configured

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,10 +1,23 @@
 import pytest
 
+from django.conf import settings
+
 from shortener.forms import URLShortenerForm
 
 
+@pytest.fixture(scope='session')
+def db_host_fixture(request):
+    original_db_host = settings.DATABASES["default"]["HOST"]
+    settings.DATABASES["default"]["HOST"] = "localhost"
+
+    def reset_db_host():
+        settings.DATABASES["default"]["HOST"] = original_db_host
+
+    request.addfinalizer(reset_db_host)
+
+
 @pytest.mark.django_db
-def test_url_shortener_form():
+def test_url_shortener_form(db_host_fixture):
     # Проверяет форму на валидность при пустом поле
     form = URLShortenerForm({'url': ''})
     assert form.is_valid() is False
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,12 +1,23 @@
 import pytest
 
-from django.urls import reverse
+from django.conf import settings
 
 from shortener.models import Link
 
 
+@pytest.fixture(scope='session')
+def db_host_fixture(request):
+    original_db_host = settings.DATABASES["default"]["HOST"]
+    settings.DATABASES["default"]["HOST"] = "localhost"
+
+    def reset_db_host():
+        settings.DATABASES["default"]["HOST"] = original_db_host
+
+    request.addfinalizer(reset_db_host)
+
+
 @pytest.mark.django_db
-def test_link_model():
+def test_link_model(db_host_fixture):
     link = Link.objects.create(
         original_url="https://www.example.com", short_hash="abcdef"
     )
